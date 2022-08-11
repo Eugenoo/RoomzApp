dbbb0e9109fb8e7423d41f6a9cf6bc307ffdd40b
Filip Tuliszkiewicz <filiptuliszkiewicz@Filips-MacBook-Pro.local>
2022-08-11 14:02:13 +0200

    initial comit

---
+++ RoomzApp/RoomzApp/RoomzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomzAppApp.swift
+//  RoomzApp
+//
+//  Created by Filip Tuliszkiewicz on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RoomzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
